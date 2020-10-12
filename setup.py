06cec599163d41220713fc1e3999f99dce9df4d5
Jollokim <joakimjensenj@gmail.com>
2020-10-12 16:01:38 +0200

    some simple unit testes

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='prosjektnavn',
+    version='0.0.1',
+    packages=['pytestings'],
+    include_package_data=True,
+    install_requires=[
+        "pytest"
+    ],
+)
