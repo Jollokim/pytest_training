240a0bc235029fb703ea6e63dfdd1fc7b22fcd13
Jollokim <joakimjensenj@gmail.com>
2020-10-12 16:02:53 +0200

    setupfile

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='prosjektnavn',
     version='0.0.1',
-    packages=['pytestings'],
+    packages=['pytesting'],
     include_package_data=True,
     install_requires=[
         "pytest"
